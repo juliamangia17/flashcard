fd9af08f16c13886f22375cc3c5b1767a27193a5
juliamangia17 <00001090988370sp@al.educacao.sp.gov.br>
2024-11-29 14:01:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    ' Rússia.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    '1889'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual é o planeta mais próximo do Sol?',
+    'O planeta mais próximo do Sol é Mercúrio.'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou o famoso quadro "Mona Lisa"?',
+    'Leonardo da Vinci.'
+)
